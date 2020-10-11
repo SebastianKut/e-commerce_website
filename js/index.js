a624106ca0f9b9d1e1cddf9828ffb2f2a3a7e9e5
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-10-11 19:42:44 +0200

    nav done

--- js/index.js
+++ js/index.js
@@ -3,8 +3,23 @@
 Navigation
 =============
  */
+const navOpen = document.querySelector('.nav__hamburger');
+const navClose = document.querySelector('.close__toggle');
+const menu = document.querySelector('.nav__menu');
 
+navOpen.addEventListener('click', () => {
+    menu.classList.add('open');
+    document.body.classList.add('active');
+    menu.style.left = '0';
+    menu.style.width = '30rem';
+});
 
+navClose.addEventListener('click', () => {
+    menu.classList.remove('open');
+    document.body.classList.remove('active');
+    menu.style.left = '-30rem';
+    menu.style.width = '0';
+});
 
 /*
 =============
